23e14038bf6288e0bad1ef975a10a9fd0ce6ad6a
Akshays <49100604+akshays-repo@users.noreply.github.com>
2019-10-17 20:32:18 +0530

    Update appbar.js

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -3,14 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
+    flexGrow: 1,},
   title: {
     flexGrow: 1,
   },
@@ -21,13 +18,12 @@ export default function ButtonAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar color="#dcdde1" position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
-            RANDOM QUOTE MACHINE 
+            QUOTE MACHINE 
           </Typography>
+          <Button href="https://github.com/akshays-repo/Random-quote-generator" color="inherit">Github</Button>
         </Toolbar>
       </AppBar>
     </div>
